d5b8c82ed975e76578ce063d62ef4dba190c17c9
MassterMax <59767630+MassterMax@users.noreply.github.com>
2022-03-31 05:03:30 +0300

    [WIP] refactoring

--- codesearch/es/metrics/codesearchnet/extract_data.py
+++ codesearch/es/metrics/codesearchnet/extract_data.py
@@ -1,4 +1,3 @@
-# todo - WIP
 from pathlib import Path
 from pprint import pprint
 
@@ -7,27 +6,7 @@ import pandas as pd
 from typing import Any, Dict, List
 
 parser = TokenParser()
-PATH_TO_DATASET = "/mnt/c/Users/maxma/Desktop/dataset/"
-
-python_files = sorted(Path(PATH_TO_DATASET).glob("**/*.gz"))
-columns_long_list = ['repo', 'path', 'url', 'code',
-                     'code_tokens', 'docstring', 'docstring_tokens',
-                     'language', 'partition']
-
-f = python_files[0]
-print(f)
-# for el in python_files:
-#     print(el)
-#     print(type(el))
-
-data = pd.read_json(f,
-                    orient='records',
-                    compression='gzip',
-                    lines=True)
-
-values = data.values.tolist()
-
-
+PATH_TO_DATASET = "/mnt/c/Users/maxma/Desktop/dataset/"  # todo remove
 DATASET_TO_ELASTIC_MAPPING = {
     "code": "function_body",
     "code_tokens": "identifiers",
@@ -35,6 +14,24 @@ DATASET_TO_ELASTIC_MAPPING = {
     "url": "location",
     "func_name": "function_name"
 }
+
+
+def dataset_to_elastic(path_to_dataset: str = PATH_TO_DATASET) -> List[Dict[str, Any]]:
+    python_files = sorted(Path(path_to_dataset).glob("**/*.gz"))
+
+    # todo now get first file, then get all files
+    file = python_files[0]
+    print(f"extract data from: {file}")
+
+    data = pd.read_json(file,
+                        orient='records',
+                        compression='gzip',
+                        lines=True)
+
+    values = data.values.tolist()
+    print(f"columns: {data.columns.tolist()}")
+
+    return [prepare_entity_to_elastic(data.columns.tolist(), value) for value in values]
 
 
 def prepare_entity_to_elastic(columns: List[str], entity: List[str]) -> Dict[str, Any]:
@@ -52,7 +49,6 @@ def prepare_entity_to_elastic(columns: List[str], entity: List[str]) -> Dict[str
     return result
 
 
-print(data.columns.tolist())
-pprint(values[0])
-prepared = prepare_entity_to_elastic(data.columns.tolist(), values[0])
-pprint(prepared)
+if __name__ == "__main__":
+    prepared_values = dataset_to_elastic()
+    pprint(prepared_values[:5])
