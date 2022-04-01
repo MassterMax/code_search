50271a458c01838ddbc01a7e956b3d0455a049ae
MassterMax <59767630+MassterMax@users.noreply.github.com>
2022-04-01 04:00:18 +0300

    Update evaluation.py

--- codesearch/es/metrics/codesearchnet/evaluation.py
+++ codesearch/es/metrics/codesearchnet/evaluation.py
@@ -1,18 +1,22 @@
 from typing import Dict, List
 
 from codesearch.es.client import ElasticSearchClient
+from codesearch.es.vs.v1 import transform_output_light
 
 
-def top_n(dataset: List[Dict[str, str]], client: ElasticSearchClient, index_name: str, top_n: int = 10):
+def top_n(dataset: List[Dict[str, str]], client: ElasticSearchClient, index_name: str, body: Dict, top_n: int = 10):
     score = 0.0
-
+    # todo [WIP]
     for item in dataset:
         query = item["query"]
         location = item["location"]
 
-        result = client.search_doc(index_name, {"query": query, "from": 0, "size": top_n})
+        result = client.instance.search(index=index_name, body=body)
+        result = transform_output_light(result)
 
-        for entity in result:
+        for i, entity in enumerate(result):
+            if i == top_n:
+                break
             if entity["location"] == location:
                 score += 1
                 break
@@ -27,28 +31,33 @@ def make_search_query(query: str,
                       docstring_weight: int = 1,
                       location_weight: int = 1,
                       function_name_weight: int = 1,
+                      prefix_length: int = 2,
+                      start: int = 0,
+                      size: int = 5
                       ) -> Dict:
     return {
-            "query": {
-                "bool": {
-                    "must": {
-                        "multi_match": {
-                            "query": query,
-                            "fields": [
-                                # exact occurrence
-                                f"identifiers^{identifiers_weight}",
-                                f"split_identifiers^{split_identifiers_weight}",
-                                f"function_body^{function_body_weight}",
-                                # meaning
-                                f"docstring^{docstring_weight}",
-                                f"location^{location_weight}"
-                                f"function_name^{function_name_weight}",
-                            ],
-                            "type": "most_fields",
-                            "fuzziness": "AUTO",
-                            "prefix_length": 2
-                        }
+        "query": {
+            "bool": {
+                "must": {
+                    "multi_match": {
+                        "query": query,
+                        "fields": [
+                            # exact occurrence
+                            f"identifiers^{identifiers_weight}",
+                            f"split_identifiers^{split_identifiers_weight}",
+                            f"function_body^{function_body_weight}",
+                            # meaning
+                            f"docstring^{docstring_weight}",
+                            f"location^{location_weight}"
+                            f"function_name^{function_name_weight}",
+                        ],
+                        "type": "most_fields",
+                        "fuzziness": "AUTO",
+                        "prefix_length": prefix_length
                     }
                 }
             }
-        }
+        },
+        "from": start,
+        "size": size
+    }
