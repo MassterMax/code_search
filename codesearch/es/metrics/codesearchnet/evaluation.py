16ac1a57739e525191c13e1d7778375bdacfd4a8
MassterMax <59767630+MassterMax@users.noreply.github.com>
2022-04-01 04:45:20 +0300

    Update evaluation.py

--- codesearch/es/metrics/codesearchnet/evaluation.py
+++ codesearch/es/metrics/codesearchnet/evaluation.py
@@ -1,21 +1,24 @@
-from typing import Dict, List
+from typing import Callable, Dict, List
 
 from codesearch.es.client import ElasticSearchClient
 from codesearch.es.vs.v1 import transform_output_light
 
 
-def top_n(dataset: List[Dict[str, str]], client: ElasticSearchClient, index_name: str, body: Dict, top_n: int = 10):
+def top_n(dataset: List[Dict[str, str]],
+          search_query_func: Callable[[str], Dict],
+          client: ElasticSearchClient,
+          index_name: str,
+          n: int = 10):
     score = 0.0
-    # todo [WIP]
     for item in dataset:
-        query = item["query"]
-        location = item["location"]
+        query = item["query"]        # feature
+        location = item["location"]  # target
 
-        result = client.instance.search(index=index_name, body=body)
+        result = client.instance.search(index=index_name, body=search_query_func(query))
         result = transform_output_light(result)
 
         for i, entity in enumerate(result):
-            if i == top_n:
+            if i == n:
                 break
             if entity["location"] == location:
                 score += 1
@@ -24,18 +27,17 @@ def top_n(dataset: List[Dict[str, str]], client: ElasticSearchClient, index_name
     return score / len(dataset)
 
 
-def make_search_query(query: str,
-                      identifiers_weight: int = 1,
-                      split_identifiers_weight: int = 1,
-                      function_body_weight: int = 1,
-                      docstring_weight: int = 1,
-                      location_weight: int = 1,
-                      function_name_weight: int = 1,
-                      prefix_length: int = 2,
-                      start: int = 0,
-                      size: int = 5
-                      ) -> Dict:
-    return {
+def make_search_query_func(identifiers_weight: int = 1,
+                           split_identifiers_weight: int = 1,
+                           function_body_weight: int = 1,
+                           docstring_weight: int = 1,
+                           location_weight: int = 1,
+                           function_name_weight: int = 1,
+                           prefix_length: int = 2,
+                           start: int = 0,
+                           size: int = 5
+                           ) -> Callable[[str], Dict]:
+    return lambda query: {
         "query": {
             "bool": {
                 "must": {
